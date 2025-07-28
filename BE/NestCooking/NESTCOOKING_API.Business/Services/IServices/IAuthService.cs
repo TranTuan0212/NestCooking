4331faf8b296d23eeb780c4630a90fe6912b29d0
Khoi <dinhkhoi.le3@gmail.com>
2025-07-28 17:46:51 +0700

    feature: login and sign up

--- BE/NestCooking/NESTCOOKING_API.Business/Services/IServices/IAuthService.cs
+++ BE/NestCooking/NESTCOOKING_API.Business/Services/IServices/IAuthService.cs
@@ -11,15 +11,7 @@ namespace NESTCOOKING_API.Business.Services.IServices
     public interface IAuthService
     {
         Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
-        Task<bool> Register(RegistrationRequestDTO registrationRequestDTO);
-        Task<(string, string)> GenerateResetPasswordToken(string identifier);
-        Task<bool> ResetPassword(ResetPasswordRequestDTO resetPasswordRequestDTO);
-        Task<bool> VerifyResetPasswordToken(string email, string token);
-        Task<string> LoginWithThirdParty(OAuth2RequestDTO oAuth2RequestDTO, ProviderLogin providerLogin);
-        Task<(string Email, string Username, string AvatarURL)> VerifyIdentifierResetPassword(string identifier);
-        Task<bool> VerifyEmailConfirmation(string email, string token);
-        Task<bool> VerifyEmailResetPassword(string email, string token);
-
         Task<(string, string)> GenerateEmailConfirmationTokenAsync(string email);
+        Task<bool> Register(RegistrationRequestDTO registrationRequestDTO);
     }
 }
